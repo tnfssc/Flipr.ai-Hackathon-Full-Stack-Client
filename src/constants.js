24b7b6e99315f9fe9eaec42f5372562fac5e8867
tnfssc <tnfssc@gmail.com>
2020-03-21 22:27:42 +0530

    done

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const serverAddress = 'http://localhost:4000/'
+//export const serverAddress = 'http://localhost:4000/'
+export const serverAddress = 'https://flipraitnfssc.azurewebsites.net/'
 export const registerAddress = serverAddress + 'register'
 export const loginAddress = serverAddress + 'login'
 export const forgotpassAddress = serverAddress + 'forgotpassword'
